79958338d227e48b4a33bbd05d4b998f059b9ca4
qvisten999 <jqjqvist@gmail.com>
2022-09-27 12:09:46 +0200

    codeMirror like textarea

--- src/lib/Editor.svelte
+++ src/lib/Editor.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import { onMount } from 'svelte';
+	import { stop_propagation } from 'svelte/internal';
 	import { markdown } from './store';
 
 	onMount(() => {
@@ -9,16 +10,27 @@
 		}
 	})
 
-	let text = '';
+	$: text = '';
+	$: editorHeight = Math.max(text.split('\n').length, 26);
+	$: numbers = Array.from(Array(editorHeight).keys());
 
 	// We should add to store instead -> support undo CMD+Z
 	function saveToStore() {
 		markdown.set(text);
 	}
 
-</script>
 
-<textarea  bind:value={text} on:input={() => saveToStore()} class="editor"></textarea>
+</script>
+<div class="editor">
+	<div class="line-numbering" style="height: calc({editorHeight * 1.5}em + 8px)">
+		<ul>
+			{#each numbers as number}
+			<li>{number}</li>
+			{/each}
+		</ul>
+	</div>
+	<textarea style="min-height: calc({editorHeight}em + 100vh)" bind:value={text} on:input={() => saveToStore()}></textarea>
+</div>
 
 <style lang="scss">
 	@import "src/consts.scss";
@@ -27,10 +39,43 @@
 		height: calc(100vh - $header-height - 2 * $default-padding);
 		background-color: $white;
 		margin: $default-padding;
+		display: flex;
+		flex-direction: row;
+		overflow: overlay;
 	}
 
 	:global(body.dark-mode) .editor {
 		background-color: $primary-color;
 		color: $white;
 	}
+
+	.line-numbering {
+		width: 35px;
+		display: flex;
+		background-color: $grey;
+	}
+
+	:global(body.dark-mode) .line-numbering {
+		background-color: $black;
+	}
+
+	ul {
+		list-style-type: none;
+		margin-left: -35px;
+		padding-top: 10px;
+	}
+	textarea {
+		width: 100%;
+		resize: none;
+		padding-top: 10px;
+		padding-left: 8px;
+		border: none;	
+		overflow: visible;
+}
+
+:global(body.dark-mode) 	textarea {
+		background-color: $primary-color;
+}
+
+
 </style>
